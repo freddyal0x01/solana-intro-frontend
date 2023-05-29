d03273f638bd7cc67942bb15f0995a38e4f27d41
freddyal <freddyal0x01@gmail.com>
2023-05-28 22:20:12 -0400

    Added solution code

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import * as Web3 from '@solana/web3.js'
 const Home: NextPage = () => {
   const [balance, setBalance] = useState(0)
   const [address, setAddress] = useState('')
+  const [isExecutable, setIsExecutable] = useState(false)
 
   const addressSubmittedHandler = (address: string) => {
     try {
@@ -16,9 +17,13 @@ const Home: NextPage = () => {
       connection.getBalance(key).then(balance => {
         setBalance(balance / Web3.LAMPORTS_PER_SOL)
       })
+      connection.getAccountInfo(key).then(info => {
+        setIsExecutable(info?.executable ?? false)
+      })
     } catch (error) {
       setAddress('')
       setBalance(0)
+      setIsExecutable(false)
       alert(error)
     }
   }
@@ -32,6 +37,7 @@ const Home: NextPage = () => {
         <AddressForm handler={addressSubmittedHandler} />
         <p>{`Address: ${address}`}</p>
         <p>{`Balance: ${balance} SOL`}</p>
+        <p>{`Is it executable? ${isExecutable ? "Yes" : "No"}`}</p>
       </header>
     </div>
   )
